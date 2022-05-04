f7ffee8ebbd9e8423dcab1be96546abb030d5b6d
charles3019 <charles3019@my.shu.ac.uk>
2022-05-04 23:27:43 +0500

    Dya02

---
+++ Day02/May04.js
@@ -0,0 +1,114 @@
+// How to create variables:
+// Creating or Declearing a variable
+// var x;
+// let y;
+
+//  Declearing and Initialization
+var k = 8;
+
+
+// How to use variables:
+//Initialization
+// x = 5;
+// y = 6;
+
+//  Declearing and Initialization
+// let z = x + y;
+// ()
+// {}
+
+// console.log("X value:", x);
+// console.log("Y value:", y);
+// console.log("Z value:", z);
+
+
+//Changing the value of x
+// Assigning value
+// x = 9;
+// console.log("X value:", x);
+
+// x = y;
+
+// console.log("X value:", x);
+
+
+// console.log("h");
+//These two are different
+// console.log('x'); // litteral
+// console.log(x); // variable
+
+
+// (5 + 6) * 10
+
+// 6 x 10 = 60 ;
+// 60 +5 = 65
+// console.log(5 + 6*10);
+
+
+// (5 +6) = 11
+// 11*10 = 110
+// console.log((5 + 6)*10);
+
+let x = 5;
+let y = 6;
+let z = 11;
+let m = 3;
+
+
+// console.log(x+y);
+let d = z/m;
+let mu = y*d;
+
+let result = x = mu;
+// console.log(d);
+// console.log("result: ",result);
+// console.log("John" + " " + "Doe");
+// console.log("John "                     + "     Doe");
+
+// console.log("John", "Doe");
+
+let john = "John";
+let doe = "Doe";
+
+let anything = john + " " + doe;
+// console.log(anything);
+
+let addNumAndStr = john + x;
+// console.log(addNumAndStr);
+
+x = 5;
+let numStr = "7";
+result = x + numStr; //Javascript joins a number and a string 
+
+// console.log("5 + 7:", result); 
+
+result = x - numStr; //bug in javascript
+// console.log("5 - 7:", result);
+
+result = x * numStr; //bug in javascript
+// console.log("5 * 7:", result);
+result = x * john;
+// console.log("5 * john:", result); // Nan (Not a Number)
+
+function addNums(x , y) {
+    let add = x + y;
+    return add;
+    
+}
+
+result = addNums(7, 8);
+// console.log("result" , result);
+
+//javaScript is Case Sensitive
+let lastname, lastName;
+lastName = "Doe";
+lastname = "Peterson";
+
+
+// let last-name; error
+let last_name;
+
+let MyNameIsJohn; //Pascal Notation
+let myNameIsJohn; //Camel Notation
+
+
