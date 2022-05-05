f5025524dc4db1d0875462108f5ea54336ec712e
charles3019 <charles3019@my.shu.ac.uk>
2022-05-05 22:45:42 +0500

    Dya03

---
+++ Day03/May05.js
@@ -0,0 +1,155 @@
+let x = 5;
+// console.log("x:", x);
+x = 9;
+
+let _x = 10;
+
+// console.log("x:", x);
+
+const y = 99;
+
+const taxRate = 0.2653723;
+const taxrate = 0.2653723;
+
+// console.log("y:", y);
+
+// y = 100;
+x = x + 5 ; // 9 + 5
+// x += 5; // shorthand
+
+// console.log("X:",x);
+
+x = 14;
+let z = 13;
+
+// console.log("z==x",z==x);
+
+
+// JavaScript Data Types
+// Number
+// String
+// 
+
+
+
+// let myName = "john";
+
+// console.log(typeof(myName));
+// console.log(typeof(x));
+
+const pi = 3.14;
+
+// console.log(typeof(pi));
+
+
+// let person = "John Doe",
+// carName = "Volvo",
+// price = 200;
+
+// let person = "John Doe";
+// let carName = "Volvo";
+// let price = 200;
+
+
+//Undefined
+
+
+let result;
+let a = 10;
+let b = 12;
+// console.log("result",result);
+
+result = a + b;
+// console.log("result", result);
+
+var g = 22;
+
+// console.log("g", g);
+
+var g;
+// console.log("g", g);
+
+
+let k = 22;
+
+// console.log("k", k);
+
+// let k;
+// console.log("k", k);
+
+// let u = 5 + 2 + 3;
+// let u = "5" + 2 + 3;
+let u = 2 + 3 + "5";
+// console.log("u", u);
+
+let c = "John" + " " + "Doe";
+// console.log("c", c);
+
+let $$$ = 2;
+// console.log($$$);
+
+let $myMoney = 5;
+
+// console.log($myMoney);
+
+{
+    let j = 99;
+
+
+    {
+        // let j = 99;
+        // console.log("inner", j);
+    }
+
+    // console.log("outer", j);
+}
+
+// console.log("outer most", j);
+
+
+// 5 sqaure 2 = 25 = 5 x 5
+// console.log(5**2);
+
+// 5 power 3 = 125 = 5 x 5 x 5
+
+// console.log("5 power 3", 5**3);
+
+
+let whatever;
+let base = 2;
+let power = 5;
+
+whatever = base ** power;
+let copy;
+// console.log("whatever",power5);
+// console.log('73 % 3 ==', 73 % 3);
+// copy = base;
+// console.log("copy: ", copy);
+// console.log("base: ", base);
+
+// // copy = base ++; //Post increament
+
+// copy = ++base; // pre increamnt
+
+
+
+// console.log("after increment");
+// console.log("copy: ", copy);
+// console.log("base: ", base);
+
+
+
+copy = base;
+console.log("copy: ", copy);
+console.log("base: ", base);
+
+copy = base -- //Post increament
+
+// copy = --base; // pre increamnt
+
+
+
+console.log("after decrement");
+console.log("copy: ", copy);
+console.log("base: ", base);
+
