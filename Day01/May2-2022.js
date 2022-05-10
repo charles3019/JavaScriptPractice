246ba3fc48f19fd5b5d02a8e4548723233b946b6
charles3019 <b9041338@my.shu.ac.uk>
2022-05-10 16:58:57 +0100

    practice on dayfile

---
+++ Day01/May2-2022.js
@@ -0,0 +1,43 @@
+//let Username = 20;
+
+//console.log(Username);
+
+
+//let userName = "bridget";
+
+//console.log("Name:", Name);
+
+
+//x = 14;
+//let z = 13;
+
+// console.log("z==x",z==x);
+
+
+//let person = "John Doe",
+// carName = "Volvo",
+// price = 200;
+
+//let person = "John Doe";
+//let carName = "Volvo";
+//let price = 200;
+
+//console.log(userName);
+
+/*let result;
+let a = 10;
+let b = 12;
+result = a + b;
+console.log("result",result);
+*/
+
+/*let x = 10;
+let y = 7;
+let result = x + y;
+
+console.log('result:' + result);
+*/
+let Anything = Bridget + " " + Oduro;
+//let Anything = "Bridget " + " Oduro";
+console.log(Anything);
+
