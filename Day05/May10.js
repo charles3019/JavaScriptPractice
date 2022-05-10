28585062bb671844844ed34588f1a1ec1b307ea7
charles3019 <charles3019@my.shu.ac.uk>
2022-05-10 22:50:36 +0500

    Dya05

---
+++ Day05/May10.js
@@ -0,0 +1,82 @@
+
+//data types 
+// let x = {num: "99"}; // Object
+// let x = {firstName:"John", lastName:"Doe"};    // Object
+// console.log("X: ",x);
+// console.log("x.firstName ",x.firstName);
+// let typeOfX = typeof(x);
+// console.log(typeof(x));
+
+
+// let answer1 = "It's alright"; 
+// console.log("before", answer1 );
+// answer1 = undefined; // Making undefined
+
+// console.log("after",answer1 );
+// let answer3 = 'He is called "Johnny"'; 
+// console.log(answer3);
+// let typeOfAnswer3 = typeof(answer3)
+// let typeOfAnswer1 = typeof(answer1)
+
+// let result = (typeOfAnswer1 == typeOfAnswer3)
+
+// console.log(result);
+
+
+// JavaScript Numbers
+// let x1 = 34.08;     // Written with decimals
+// let x2 = 34; 
+
+// console.log("A number with decimal", x1);
+// console.log("A number without decimal", x2);
+
+// JavaScript Arrays
+// const cars = ["Saab", "Volvo", "BMW"]; //declearing
+// // const cars = ["Saab", "Volvo", 54];
+
+// console.log(cars); // outputing all elements at the same time.
+// console.log(cars[2]); // accessing an element of the array
+
+
+//Function
+
+// function myFunction(p1, p2) {
+
+//     return p1 * p2;   // The function returns the product of p1 and p2
+//   }
+
+//   function myFunction(p1, p2) {
+//     let result = p1 * p2;
+//     return result;   // The function returns the product of p1 and p2
+//   }
+
+
+// let x = myFunction(6, 8); // let x = 48
+// let y = myFunction(9, 8);
+// let z = myFunction(6, 6);
+// console.log("X", x);
+// console.log("Y", y);
+// console.log("Z", z);
+
+// let a = 6;
+// let b = 8;
+
+// let k = myFunction(a, b);
+
+// console.log("K:", k);
+
+
+// function myFunction(p1, p2) {
+//     let result = p1 * p2;
+//     console.log("variable1", p1);
+//     console.log("variable2", p2);
+//     console.log("Product of both", result);    
+// }
+
+// myFunction(6, 8); // let x = 48
+// myFunction(9, 8);
+// myFunction(6, 6);
+
+// let x = myFunction(6, 6);
+
+// console.log("X", x);
