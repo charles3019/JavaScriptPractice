977348827074dba8673b843fb8f7c2801cc813be
charles3019 <charles3019@my.shu.ac.uk>
2022-05-10 09:08:45 +0500

    Dya04

---
+++ Day04/May09.js
@@ -0,0 +1,221 @@
+// let x = 3;
+// let y = 10;
+// console.log("before");
+// console.log("x", x);
+// console.log("y", y);
+
+// x = y;
+
+// console.log("after");
+// console.log("x", x);
+// console.log("y", y);
+
+// let x = 3;
+// let y = 10;
+
+// console.log("before");
+// console.log("x", x);
+// console.log("y", y);
+
+// x += y; // x = x + y; (x = 5 + 10)
+
+
+// console.log("after");
+// console.log("x", x);
+// console.log("y", y);
+
+
+
+// let x = 10;
+// let y = 3;
+
+// console.log("before");
+// console.log("x", x);
+// console.log("y", y);
+
+// x -= y; // x = x + y; (x = 5 + 10)
+
+
+// console.log("after");
+// console.log("x", x);
+// console.log("y", y);
+
+
+// let x = 10;
+// let y = 3;
+
+// console.log("before");
+// console.log("x", x);
+// console.log("y", y);
+
+// x *= y; // x = x + y; (x = 5 + 10)
+
+
+// console.log("after");
+// console.log("x", x);
+// console.log("y", y);
+
+
+
+// let x = 15;
+// let y = 3;
+
+// console.log("before");
+// console.log("x", x);
+// console.log("y", y);
+
+// x /= y; // x = x + y; (x = 5 + 10)
+
+
+// console.log("after");
+// console.log("x", x);
+// console.log("y", y);
+
+
+
+
+// let x = 16;
+// let y = 3;
+
+// console.log("before");
+// console.log("x", x);
+// console.log("y", y);
+
+// x %= y; // x = x + y; (x = 5 + 10)
+
+
+// console.log("after");
+// console.log("x", x);
+// console.log("y", y);
+
+// let x = 16;
+// let y = 3;
+
+// console.log("before");
+// console.log("x", x);
+// console.log("y", y);
+
+// x **= y; // x = x + y; (x = 5 + 10)
+
+
+// console.log("after");
+// console.log("x", x);
+// console.log("y", y);
+
+// let text1 = "What a very ";
+// let text2 = "nice day";
+
+// console.log("before");
+// console.log("text1", text1);
+// console.log("text2", text2);
+
+// // text1 += "nice day";
+// text1 += text2;
+// console.log("after");
+// console.log("text1", text1);
+// console.log("text2", text2);
+
+
+
+// JavaScript Comparison Operators
+
+
+// let x = 3;
+// let y = 10;
+
+// let z = (x===y);
+
+// console.log("z",z);
+
+
+
+// let x = "10";
+// let y = 10;
+
+// let z = (x==y);
+
+// console.log("z",z);
+
+// let x = "10";
+// let y = 10;
+
+// let z = (x===y);
+
+// console.log("z",z);
+
+
+// let x = "9";
+// let y = 10;
+
+// let z = (x!=y);
+
+// console.log("z",z);
+
+
+// let x = "10";
+// let y = "10";
+
+// let z = (x!==y);
+
+// console.log("z",z);
+
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+
+// let x = 9;
+// let y = 10;
+
+// let z = (x >= y); 
+
+// console.log("z",z);
+
+
+// let x = 11;
+// let y = 10;
+
+// let z = (x <= y); 
+
+// console.log("z",z);
+
+// let x = true;
+// let y = false;
+
+// Logical Operators
+
+// let z = (x && y)
+// console.log("z",z);
+
+// let x = true;
+// let y = true;
+
+
+// let z = (x || y)
+// console.log("z",z);
+
+
+
+// let x = false;
+
+
+
+// let z = !x
+// console.log("x",x);
+// console.log("z",z);
+
+
+// let x = false;
+// let h;
+// let n = 99;
+// let z = !x;
+
+// console.log("typeof x", typeof(x));
+// console.log("typeof z", typeof(z));
+// console.log("typeof h", typeof(h));
+// console.log("typeof n", typeof(n));
+
+
+// z = x + y;
+
+
