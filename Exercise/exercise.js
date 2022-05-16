3b4631615b605ffcdc117d906f72bf717fe54b5d
charles3019 <b9041338@my.shu.ac.uk>
2022-05-16 14:04:42 +0100

    exercise

---
+++ Exercise/exercise.js
@@ -0,0 +1,19 @@
+//write a program that divides two  variables and finds the remainder
+
+x = 17;
+a = 2;
+
+//let results = x / a;
+let remainder = x % a;
+
+//console.log("Result:", results);
+console.log("remainder:", remainder);
+
+
+// write a programthat gets temperature from the user in celsius and convert its to fahrenheit using the formula F = 9/5*C+32.
+
+//write a program that comnpare two number and find whether both are equal
+
+// write a program that compare numbers and finds if the second is the square of the first of the first.
+
+// write a program that has two variables "Salary" and "Grade". it add 50% bonus if the grade is greater than 15. it adds 25% bonus if the grade is 15 or less and then dispay the salary.
