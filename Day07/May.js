868b18e5beceeaecd434a7cc77fd212bbc28f45e
charles3019 <charles3019@my.shu.ac.uk>
2022-05-16 23:17:33 +0500

    Dya07

---
+++ Day07/May.js
@@ -0,0 +1,44 @@
+// const cars = ["Saab", "Volvo", "BMW"];
+// // console.log("before:",cars);
+// cars[0] = "Opel";
+
+// // console.log("after:",cars);
+// cars[2] = "JJ";
+
+// // console.log("last:",cars);
+
+// console.log("Typesof:",typeof(cars));
+
+// let myArray = [];
+// const person = {firstName:"John", lastName:"Doe", age:46};
+
+
+
+// myArray[0] = person;
+// myArray[1] = function myFunction(p1, p2) {
+//     return p1 * p2;   // The function returns the product of p1 and p2
+//   };
+// myArray[2] = cars;
+
+// console.log(myArray);
+const fruits = ["Banana", "Orange", "Apple", "mango"];
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+// fruits.reverse();
+// fruits.sort();
+
+console.log(fruits);
+// console.log("reverse: ", fruits);
+// if (result === -1) {
+//     console.log("BMW" ,"is not present in Array");
+// }
+// else{
+//     console.log("Banana", "is present in Array");
+
+// }
+// let fruit = fruits[fruits.length - 1];
+
+fruits[4] = "Lemon"; 
+
+console.log("Fruits:", fruits);
