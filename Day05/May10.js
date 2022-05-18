5bfcbeb9605061ad5b6e759c236623dcadb1aa77
charles3019 <b9041338@my.shu.ac.uk>
2022-05-18 22:52:53 +0100

    update

--- Day05/May10.js
+++ Day05/May10.js
@@ -6,13 +6,32 @@
 // console.log("x.firstName ",x.firstName);
 // let typeOfX = typeof(x);
 // console.log(typeof(x));
+/*
+let b = {num: "99"}; // Object
+let c = {firstName:"Charles", lastName:"Agyemang"}; // Object
+console.log("b", b);
+console.log("firstName:",c.firstName, "lastname:",c.lastName);
+
+let typeofC = typeof(c);
+console.log(typeof(c));
+*/
 
 
 // let answer1 = "It's alright"; 
 // console.log("before", answer1 );
 // answer1 = undefined; // Making undefined
 
+let answer = "thats great";
+
+//console.log("before:", answer);
+//answer = undefined;
+//console.log("after:", answer);
+
+
 // console.log("after",answer1 );
+
+
+
 // let answer3 = 'He is called "Johnny"'; 
 // console.log(answer3);
 // let typeOfAnswer3 = typeof(answer3)
@@ -22,17 +41,32 @@
 
 // console.log(result);
 
+//let answer2 = 'I have a dreaming called "software engineer"';
+//console.log(answer2);
+//let typeOfAnswer2 = typeof(answer2)
+
+//console.log(typeOfAnswer2);
+//let typeofAnswer = typeo(answer)
+
 
 // JavaScript Numbers
 // let x1 = 34.08;     // Written with decimals
 // let x2 = 34; 
+ 
 
 // console.log("A number with decimal", x1);
-// console.log("A number without decimal", x2);
+//console.log("A number without decimal", x2);
+
 
 // JavaScript Arrays
 // const cars = ["Saab", "Volvo", "BMW"]; //declearing
 // // const cars = ["Saab", "Volvo", 54];
+
+//const books = ["getrich", "power", "sexy billionaire"];
+//const books = ["getrich", "power", "sexy", 45];
+//console.log(books);
+//console.log(books[2]);
+
 
 // console.log(cars); // outputing all elements at the same time.
 // console.log(cars[2]); // accessing an element of the array
@@ -80,3 +114,4 @@
 // let x = myFunction(6, 6);
 
 // console.log("X", x);
+
