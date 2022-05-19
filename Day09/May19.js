c2b51859c1025b5ccf1e3159ecf00cd93c67cd85
charles3019 <charles3019@my.shu.ac.uk>
2022-05-19 22:36:26 +0500

    Dya09

---
+++ Day09/May19.js
@@ -0,0 +1,122 @@
+// console.log("Hello World");
+
+// function myFunction(p1, p2) {
+
+//     return p1 * p2;   // The function returns the product of p1 and p2
+//   }
+
+
+// function multiply(p1, p2){
+//     if (p1 > 0) {
+//         // let result = myFunction(p1, p2)
+//         let result = p1 * p2;
+//         console.log(p1, "*", p2 , "=", result);
+//     }else{
+//         console.log("p1 is 0");
+//     }
+// }
+
+// multiply(5, 8);
+
+// switch (x) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//   }
+
+
+// let x = 0;
+//  x = 2;
+
+// if (x === 0) {
+//     console.log("The day is Sunday");
+// }
+// else if (x === 1) {
+//     console.log("The day is Monday");
+//     console.log("The day is Monday");
+//     console.log("The day is Monday");
+// }
+// else if ( x === 2){
+//     console.log("The day is Tuesday");
+// }
+// else if (x === 3) {
+//     console.log("The day is Wednesday");
+// }
+// else if (x === 4) {
+//     console.log("The day is Thursday");
+// }
+// else{
+//     console.log("The Day is Undefined");
+// }
+
+// let x = 9;
+
+// switch (x){
+//     case 0:
+//     console.log("The day is Sunday");
+//     break;
+//     case 1:
+//     console.log("The day is Monday");
+//     console.log("The day is Monday");
+//     console.log("The day is Monday");
+//     break;
+//     case 2:
+//     console.log("The day is Tuesday");
+//     break;
+//     case 3:
+//     console.log("The Day is Wednesday");
+//     break;
+//     case 4:
+//     console.log("The day is Thursday");
+//     break;
+//     default:
+//     console.log("The Day is Undefined");
+// }
+
+// let result = 2 * 1;
+// console.log(2, "*", 1 , "=", result);
+// result = 2 * 2;
+// console.log(2, "*", 2 , "=", result);
+// result = 2 * 3;
+// console.log(2, "*", 3 , "=", result);
+// result = 2 * 4;
+// console.log(2, "*", 4 , "=", result);
+// result = 2 * 5;
+// console.log(2, "*", 5 , "=", result);
+
+// for (let i = 1; i < 10; i++) {
+//     let result = 2 * i;
+//     console.log(2, "*", i , "=", result);
+// }
+
+// console.log("Out of loop");
+// let i = 10;
+// // let condition = i < 10;
+// // console.log(condition);
+// while (i < 10) {
+//     let result = 2 * i;
+//     console.log(2, "*", i , "=", result);
+
+//     i++;
+// }
+
+// let i = 10;
+
+// do {
+//     let result = 2 * i;
+//     console.log(2, "*", i , "=", result);
+//     i++;
+    
+// } while (i < 10);
