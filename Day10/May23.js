0d72ca189beddc680df8c17e45cf2d28222d155f
charles3019 <charles3019@my.shu.ac.uk>
2022-05-23 23:40:35 +0500

    Dya10

---
+++ Day10/May23.js
@@ -0,0 +1,82 @@
+// let x = 0;
+
+// while(x < 0){
+//     // if (x === 5) {
+//     //     console.log("The number is", x);
+//     // }
+//     console.log("Inside loop",x);
+//     // x++;
+//     x--; 
+// }
+
+// do {
+//     console.log("Inside loop",x);
+//     x--; 
+// } while (x >= 0);
+
+
+// const person = {fname:"John", lname:"Doe", age:25};
+// // console.log(person);
+// for (let y in person) {
+//     console.log("y",y);
+
+//     if (person[y] === "Doe") {
+//         person[y] = "Zada";
+//     }
+
+//     // console.log(person[y]);
+// }
+// console.log(person);
+
+// const numbers = [45, 4, 9, 9, 9, 9, 25];
+
+// for ( let y of numbers){
+//     if (y === 9) {
+//         y = 66; 
+//     }
+//     console.log("y",y);
+
+// }
+// console.log(numbers);
+
+
+const mySet = new Set();
+mySet.add(99);
+mySet.add(88);
+mySet.add(77);
+mySet.add(5);
+
+
+
+
+console.log(mySet);
+
+// mySet.delete(77);
+// let setHas = mySet.has(44)
+
+if (mySet.has(77)) {
+ console.log("it has 77");
+    
+}
+// console.log("setHas ", setHas);
+
+// for (let y in numbers) {
+//     // console.log("y",y);
+//     if (y == 2) {
+//         numbers[y] = 66;
+//     }
+//     console.log(numbers[y]);
+
+//     // console.log(person[y]);
+// }
+
+// for(let y = 0; y <= person.fname = []; y++){    
+//      console.log(person[y]);
+//  }
+
+// console.log(text);
+// for (let x in person) {
+//   text += person[x];
+// }
+
+
