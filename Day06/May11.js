de85acbe5a4e7dd60117794d96dc8e5c6989976f
charles3019 <b9041338@my.shu.ac.uk>
2022-05-25 07:53:46 +0100

    work esercise

--- Day06/May11.js
+++ Day06/May11.js
@@ -11,34 +11,76 @@
 //     }
 //   }
 
-//  function mainFunction() {
-//     let num1 = 99;
-//     let num2 = 88;
-//     console.log("Addition of num1 =",num1, "num2 =" ,num2, "is" );
-//     let result = addition(num1, num2);
-//     console.log(result);
-//     let num3 = 77;
-//     console.log("Addition of num1 =",num1, "num2 =" ,num2, "num3 =" ,num3, "is" );
-//     result = addition(num1, num2, num3);
-//     console.log(result);
-
-//   }
 
 
+function subtraction(p1, p2, p3) {
+  if (p3 == undefined){
+    return p1 - p2; // The function returns the product
 
+  }else if (p2 == undefined){
+
+  }
+  else{
+    return p1 - p2 - p3; // The function returns the product of p1 - p2
+  }
+}
+
+let result = subtraction(9,9,7);
+
+console.log(result);
+
+
+/*
+
+function mainFunction() {
+    let num1 = 99;
+   let num2 = 88;
+    console.log("Addition of num1 =",num1, "num2 =" ,num2, "is" );
+    let result = 99 + 88;
+     console.log(result);
+     return result
+
+ }
+ 
+ mainFunction();
+*/
+
+/*
+function add() {
+  let num1 = 23;
+  let num2 = 28;
+  console.log("sum of num1 ", num1, "num2",num2,"is");
+  let result = num2 + num2;
+   console.log(result);
+   return result;
+}
+ 
+add();
+ */
+/*
+function add(arguments) {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+  }
+  return sum;
+}
+console.log(add([1, 2])); // 3
+console.log(add([1, 2, 3, 4, 5])); // 15
+*/
 // mainFunction();
 
 
 //If else conditions
 
+/*
+ let time = 21;
 
-// let time = 21;
-
-// if (time < 10) {
-//     greeting = "Good morning";
-//     console.log(greeting);
-//   }
-  
+if (time < 10) {
+   greeting = "Good morning";
+     console.log(greeting);
+  }
+  */
 //   else if (time < 20) {
 //     greeting = "Good day";
 //     console.log(greeting);
@@ -50,9 +92,22 @@
 
 //   }
 
+/*
+let time = -10;
+
+if (time > 10) {
+  greeting = "good morning";
+  console.log(greeting);
+}else if (time <10) {
+  greeting = "good Afternoon";
+  console.log(greeting);
+}else {
+  greeting = "good Night";
+}
+*/
 //Objects with Method
 
-  const person = {
+ /* const person = {
     firstName: "John",
     lastName : "Doe",
     id       : 5566,
@@ -64,3 +119,19 @@
 // console.log(person.fullName());
 let name1 = person.fullName();
 console.log(name1);
+*/
+/*
+const animals = {
+  dog: "scooby",
+  cat: "garfield",
+  lion: "Musafa",
+  animalNames: function() {
+    return this.dog + " " + this.cat;
+  }
+}
+
+console.log(animals.animalNames());
+let name2 = animals.animalNames();
+console.log(name2);
+
+*/
