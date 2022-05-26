756f6b4873b5851505ee2a81c5bc793c8ead9556
charles3019 <b9041338@my.shu.ac.uk>
2022-05-26 12:06:07 +0100

    practise

--- Day07/May.js
+++ Day07/May.js
@@ -23,8 +23,17 @@ console.log("Typesof:", typeof(books));
 // let myArray = [];
 // const person = {firstName:"John", lastName:"Doe", age:46};
 
+/*let myArray = [];
+const book = {title:"Happy sext millionaire", Author:"Steven Bratlet", Year:"2021"};
 
+myArray[0] = book;
+myArray[1] = function myFunction(p1,p2) {
+  return p1 + p2;
+};
 
+myArray[2] = book;
+console.log("myArray:",myArray);
+*/
 // myArray[0] = person;
 // myArray[1] = function myFunction(p1, p2) {
 //     return p1 * p2;   // The function returns the product of p1 and p2
@@ -32,9 +41,33 @@ console.log("Typesof:", typeof(books));
 // myArray[2] = cars;
 
 // console.log(myArray);
+
 //const fruits = ["Banana", "Orange", "Apple", "mango"];
 
 //const cars = ["Saab", "Volvo", "BMW"];
+
+//const books = ["power", "happy sexy millionaire", "think and grow rich"];
+
+const games = ['Fifa2022', "GTA", "call of duty"];
+
+//books.reverse();
+//books.sort();
+//games.reverse();
+//games.pop();
+//console.log(books, games);
+
+if (games == -1) {
+    console.log("GTA", "is not present in Array" ); 
+} else{
+    console.log("call of duty", "is present in Array");
+}
+
+let game = games[games.length  -1];
+
+games[3] = "The Bible";
+
+console.log("games", games);
+
 
 // fruits.reverse();
 // fruits.sort();
