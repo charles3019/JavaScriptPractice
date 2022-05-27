91c4580cb748f2dee65dc89bf46dd53d2546aed0
charles3019 <b9041338@my.shu.ac.uk>
2022-05-27 14:57:27 +0100

    May19 work

--- Day09/May19.js
+++ Day09/May19.js
@@ -1,5 +1,14 @@
 // console.log("Hello World");
+// console.log("Ciao Charles")
 
+// function myFunction(p1, p2) {
+
+//     return p1 * p2; //this function return the product of p1 and p2 
+// }
+
+
+// result = myFunction(2,5)
+// console.log(result);
 // function myFunction(p1, p2) {
 
 //     return p1 * p2;   // The function returns the product of p1 and p2
@@ -15,6 +24,19 @@
 //         console.log("p1 is 0");
 //     }
 // }
+
+// function multiply(p1, p2 ) {
+//     if (p1 > 0) {
+//         // let result = myFunction(p1, p2)
+//         let result = p1 * p2;
+//         console.log(p1, "*",p2 , "=", result);
+//     }else{
+//         console.log("p1 is 0");
+//     }
+// }
+
+// multiply(6,9);
+
 
 // multiply(5, 8);
 
@@ -61,6 +83,27 @@
 //     console.log("The Day is Undefined");
 // }
 
+// let b = 0;
+// b = 9;
+
+// if (b === 0) {
+//     console.log("This day is sunday");
+// }else if (b === 1) {
+//     console.log("This day is monday");
+// }else if (b === 2) {
+//     console.log("This day is Tuesday");
+// }else if (b === 3) {
+//     console.log("This day is Wednesday");
+// }else if (b === 5) {
+//     console.log("This day is Thurdays");
+// }else if (b === 6) {
+//     console.log("This day is Friday");
+// }else if (b === 7) {
+//     console.log("This day is saturday");
+// }else {
+//     console.log("undefine");
+// }
+
 // let x = 9;
 
 // switch (x){
@@ -85,6 +128,38 @@
 //     console.log("The Day is Undefined");
 // }
 
+// let b = 1;
+
+// switch (b) {
+//     case 0:
+//         console.log("The day is Monday");
+//         console.log("The day is Monday");
+//         console.log("The day is Monday");
+//         break;
+//         case 1:
+//         console.log("The day is Tuesday");
+//         break;
+//         case 2:
+//         console.log("The day is wednesday");
+//         break;
+//         case 3:
+//         console.log("The day is Thursday");
+//         break;
+//         case 4:
+//         console.log("The day is Friday");
+//         break;
+//         case 5:
+//         console.log("The day is Saturday");
+//         break;
+//         case 6:
+//         console.log("The day is Sunday");
+//         break;
+//         default:
+//         console.log("The day is undefine");
+// }
+
+
+
 // let result = 2 * 1;
 // console.log(2, "*", 1 , "=", result);
 // result = 2 * 2;
@@ -96,12 +171,23 @@
 // result = 2 * 5;
 // console.log(2, "*", 5 , "=", result);
 
+// let result = 3 * 2;
+// console.log(3, "*", 2 , "=", result);
+
+
 // for (let i = 1; i < 10; i++) {
 //     let result = 2 * i;
 //     console.log(2, "*", i , "=", result);
 // }
 
-// console.log("Out of loop");
+// for (let b = 1; b < 20; b++) {
+//     let result = 7 * b;
+//     console.log(7, "*", b, "=", result);
+// }
+
+//  console.log("Out of loop");
+
+
 // let i = 10;
 // // let condition = i < 10;
 // // console.log(condition);
@@ -112,6 +198,15 @@
 //     i++;
 // }
 
+// let v = 13;
+// let condition = v < 14;
+//  console.log(condition);
+// while (v < 14) {
+//     let result = 3 * v;
+//     console.log(3, "*", v, "=", result);
+//     v++
+// }
+
 // let i = 10;
 
 // do {
@@ -120,3 +215,11 @@
 //     i++;
     
 // } while (i < 10);
+
+
+let v = 7
+do{
+    let result = 2 * v;
+    console.log(2, "*", v , "=", result)
+    v++;
+} while (v < 30);
