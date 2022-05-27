eef811e3a5fd4fe39675478019ba98b1039472a4
charles3019 <b9041338@my.shu.ac.uk>
2022-05-27 13:11:50 +0100

    day8 exercise

--- Day08/May18.js
+++ Day08/May18.js
@@ -1,17 +1,36 @@
 // let text = "We are the so-called \"Vikings\" from the north.";
 // let text= 'It\'s alright. \n This another line.';
 
-// console.log(text);
+//let text = "We are the  \"Gladiators\" from Rome. ";
+//let text = 'it\'s okay. \n Just another line.';
 
-let str = "Apple, Banana, Kiwi";
-let part = str.slice(15, 19);
+ //console.log(text);
 
-// console.log(part);
+ //let str = "Dog, cat, Monkey";
+  //let part = str.slice(4,8)
+
+//let str = "Apple, Banana, Kiwi";
+//let part = str.slice(15, 19);
+ //console.log(part);
+
+
 // let text1 = "      Hello World!      ";
 // let text2 = text1.trim();
 // console.log(text2);
 
+//let text1 = "  Hey sexy!         ";
+//let text2 = text1.trim();
+//console.log(text1);
+ 
 // let numb = 5;
+// console.log("Before", numb);
+// console.log("typeof", typeof(numb));
+
+// let text = numb.toString();
+// console.log("After", text);
+// console.log("typeof", typeof(text));
+
+// let numb = 22;
 // console.log("Before", numb);
 // console.log("typeof", typeof(numb));
 
@@ -26,9 +45,17 @@ let part = str.slice(15, 19);
 
 // console.log(x);
 
+// let b = 999999999999999999;
+// console.log(b);
+
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // console.log(fruits);
 // let fruit = fruits.pop();
+
+// const cars = ["BMW", "FERRARI", "LAMBORGHINI"];
+// console.log(cars);
+// let car = cars.pop();
+// console.log(cars);
 
 // console.log(fruits);
 
@@ -39,31 +66,70 @@ let part = str.slice(15, 19);
 // let miliTime = d.getTime();
 
 // console.log(miliTime);
+//  const d = new Date(0);
+// const d = new Date("October 13, 2021 11:13:00");
+// const d = new Date("October 13, 2021 11:13:00");
+// let milliTime = d.getTime("11:22Friday, 27 May 2022 (BST)Time in Sheffield, UK");
+// console.log(d);
+
+// let day;
+// let x = new Date().getDay()
+// console.log(x);
+
+// switch (x) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
+//console.log(day);
+
+
 let day;
-let x = new Date().getDay()
-console.log(x);
+let b = new Date().getDay()
+console.log(b);
 
-switch (x) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
+
+switch (b) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
     case 2:
-       day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-  }
+      day = "Tuesday";
+      break;  
+  case 3:
+        day = "Wednesday";
+        break;
+   case 4:
+        day = "Thursday";
+        break;
+        case 5:
+          day = "Friday";
+          break;
+          case 6:
+    day = "Saturday";
+} 
 
-  console.log(day);
+console.log(day);
+
+
+  
