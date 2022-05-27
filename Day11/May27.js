b23a95be12fb7ee32464b92c13b6d638b9f2eecc
charles3019 <charles3019@my.shu.ac.uk>
2022-05-27 22:44:59 +0500

    Day11

---
+++ Day11/May27.js
@@ -0,0 +1,125 @@
+// let num = 77;
+// let num2 = 51;
+
+
+// console.log("The number is " + num);
+// console.log("The number is" , num);
+
+// This is know template string
+// console.log(`The number is ${num}`);
+// console.log(`The addition is ${num * num2}`);
+
+// const numbers = [45, 4, 9, 9, 8, 9, 25, "name"];
+
+//Outputs index of an array
+// for (x in numbers){
+//     console.log(x);
+// }
+
+
+//Outputs elemets of an array
+// for (x of numbers) {
+//     console.log(x);
+// }
+
+
+// const numbers = [45, 4, 9, 9, 8, 9, 25, "name"];
+
+// //Outputs elemets as well index of an array
+// numbers.forEach(function myfunction(x, index, numbers) {
+//     // console.log("x:",x, "index:", index, "numbers",numbers);
+//     // console.log("x:", x, "index:", index);
+//     console.log("index:", index, "x:",x,);
+    
+// });
+
+// console.log(Math.PI);
+// let num = 9.4;
+
+// console.log(Math.round(num));
+
+// let num = '4'**3;
+// console.log(num);
+
+// let num2 = Math.pow(4,3);
+// console.log(num2);
+
+// let num = 16;
+
+// let sqt = Math.sqrt(num);
+// let num3 = 6;
+// let result = num3 * sqt;
+//---------- 6 * 4
+// let result = num3 * Math.sqrt(num);
+
+// console.log(result);
+
+// Absolute values
+// let num = -725.98;
+// let absolut = Math.abs(num)
+// console.log("num", num, "abolut", absolut);
+
+
+// let num1 = 725.98;
+// let absolute = Math.abs(num1)
+// console.log("num1", num1, "absolute", absolute);
+
+
+// let negNum = -765.875;
+// console.log("Before:", negNum);
+// negNum = Math.abs(negNum);
+// console.log("After:", negNum);
+
+// let num1 = 7.01;
+
+// let num2 = Math.ceil(num1)
+
+// console.log("num2", num2);
+
+// let num1 = 7.99;
+
+// let num2 = Math.floor(num1)
+
+// console.log("num2", num2);
+
+
+// let minimum = Math.min(4, 8 , 9 , 155, 83, -7, -99);
+
+// console.log("minimum", minimum);
+
+// let maximum = Math.max(4, 8 , 9 , 155, 83, -7, -99);
+
+// console.log("maximum", maximum);
+
+// let random = Math.random();
+
+// console.log("random", random);
+
+// let dec = random * 100;
+// console.log("dec", dec);
+
+// let roundOff = Math.round(dec);
+
+// console.log("Result:", roundOff);
+
+
+// let random = Math.random();
+
+// console.log("random", random);
+
+// random = random * 100;
+// console.log("random", random);
+
+// random = Math.round(random);
+
+// console.log("random:", random);
+
+// let num1 = 999.645436;
+// console.log("num1", num1);
+// let truncated = Math.trunc(num1);
+// console.log("truncated", truncated);
+
+// let num2 = -7756.898;
+// console.log("num2", num2);
+// truncated = Math.trunc(num2);
+// console.log("truncated", truncated);
