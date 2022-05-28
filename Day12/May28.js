c4ba4ac70e2a28b120a2ff33a26046553335531f
charles3019 <charles3019@my.shu.ac.uk>
2022-05-28 18:31:21 +0500

    Day12

---
+++ Day12/May28.js
@@ -0,0 +1,67 @@
+// function demoFunction(parameter1, parameter2) {
+//     console.log(parameter1);
+//     console.log(parameter2);
+
+// }
+
+// demoFunction(5, 7)
+
+// let text = "Visit W3Schools!";
+// let n = text.search("w3schools");
+// console.log(n);
+
+// let text = "Visit W3Schools";
+// let n = text.search(/w3schools/i);
+// console.log(n);
+
+
+// let text = "Visit microsoft!";
+// console.log(text);
+// let result = text.replace("Microsoft", "W3Schools");
+
+// console.log(result);
+
+// let text = "Visit microsoft! microsoft";
+// console.log(text);
+// let result = text.replace(/Microsoft/ig, "W3Schools");
+// let result = text.replace(\d, "W3Schools");
+
+// const pattern = /e/;
+// pattern.test("The best things in life are free!");
+// console.log(result);
+
+// let text = "Do a multiline search for \nis at the beginning \nis of each line in a string";
+// console.log(text);
+// let result = text.match(/^is/m);
+// console.log(result);
+
+// let text = "1345672289";
+// let text = "jahLISANMXNASASUFGHHXJASK";
+
+// let result = text.match(/[h]/g);
+// let text = "re, green, red, green, gren, gr, blue, yellow";
+// let result = text.match(/(red|green|gr)/g);
+// console.log(result);
+
+// let text = "str 134 house 753";
+// let result = text.match(/\d/g);
+// // let result = text.match(/\d/);
+
+// console.log(result);
+
+
+
+//Search White Spaces
+// let text = "Is this all there is?";
+// let result = text.match(/\s/g);
+// let result = text.match(/\s/);
+// console.log(result);
+
+// kelogram
+// kelometer
+let text = "HELLONOT HELLO, LOOK AT YOU!"; 
+// let result = text.search(/\bYOU/);
+let result = text.search(/LO\b/);
+
+console.log(result);
+
