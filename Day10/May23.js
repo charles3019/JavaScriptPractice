3df748b652e0c7b8f9c24e239d97a13f7e2e99b9
charles3019 <b9041338@my.shu.ac.uk>
2022-05-30 18:40:11 +0100

    day10 update

--- Day10/May23.js
+++ Day10/May23.js
@@ -9,10 +9,26 @@
 //     x--; 
 // }
 
+// let b = 10;
+
+// while(b > 8){
+//     if (b === 10) {
+//         console.log("The number is", b);
+//     }
+//     console.log("Inside loop",b);
+//      b++;
+//     //  b--;
+// }
+
 // do {
 //     console.log("Inside loop",x);
 //     x--; 
 // } while (x >= 0);
+
+// do {
+//     console.log("Inside loop",b);
+//     b--;
+// } while (b >= 0);
 
 
 // const person = {fname:"John", lname:"Doe", age:25};
@@ -28,6 +44,20 @@
 // }
 // console.log(person);
 
+const nursestuff = {fname:"Bridget", lname:"Oduro", age:22};
+// console.log(nursestuff);
+// for (let x in nursestuff) {
+//     console.log("X",x);
+
+//     if (nursestuff[x] === "Oduro") {
+//         nursestuff[x] = "Agyemang";
+//     }
+
+//     console.log(nursestuff[x]);
+// }
+
+// console.log(nursestuff);
+
 // const numbers = [45, 4, 9, 9, 9, 9, 25];
 
 // for ( let y of numbers){
@@ -39,35 +69,50 @@
 // }
 // console.log(numbers);
 
+// const numbers = [34, 4, 5 ,7,3,23];
 
-const mySet = new Set();
-mySet.add(99);
-mySet.add(88);
-mySet.add(77);
-mySet.add(5);
+// for (let v of numbers) {
+//     if(v === 7) {
+//         v = 77;
+//     }
+//     console.log("v", v);
+// }
 
+// console.log(numbers);
 
+// const mySet = new Set();
+// mySet.add(99);
+// mySet.add(88);
+// mySet.add(77);
+// mySet.add(11);
+// mySet.add(11);
 
+// console.log(mySet);
+// let setHas = mySet.has(77)
+// console.log(setHas);
+//  mySet.delete(77);
+//  mySet.clear();
+//  console.log(mySet);
+//  let setHas = mySet.has(11)
 
-console.log(mySet);
+// if (mySet.has(77)) {
+//  console.log("it has 77");
 
-// mySet.delete(77);
-// let setHas = mySet.has(44)
-
-if (mySet.has(77)) {
- console.log("it has 77");
-    
-}
+// }
 // console.log("setHas ", setHas);
 
+// if (mySet.has(11)) {
+//     console.log("it has 11");
+// }console.log("setHas", setHas)
+
 // for (let y in numbers) {
-//     // console.log("y",y);
+//     console.log("y",y);
 //     if (y == 2) {
 //         numbers[y] = 66;
 //     }
 //     console.log(numbers[y]);
 
-//     // console.log(person[y]);
+//     console.log(person[y]);
 // }
 
 // for(let y = 0; y <= person.fname = []; y++){    
@@ -78,5 +123,3 @@ if (mySet.has(77)) {
 // for (let x in person) {
 //   text += person[x];
 // }
-
-
